53c59e14cf955bc25fafd33c415f976cdd630e14
johnreysilverio <johnrey.silverio.p@gmail.com>
2025-05-21 17:01:57 +0800

    fixed footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -8,7 +8,7 @@ import {
   FaInstagram,
   FaEnvelope,
 } from "react-icons/fa";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const Footer = () => {
   const handleSmoothScroll = (id: string) => {
@@ -17,85 +17,103 @@ const Footer = () => {
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
+  const links = ["HOME", "ABOUT", "CRAFT", "CAREER", "CONTACT"];
 
   return (
-    <div className="w-full bg-background2 flex justify-center border-border2 border-t-2 pt-5 transition-all duration-200">
-      <div className="flex flex-col items-center justify-center gap-5 w-full">
-        <div className="flex flex-col text-center gap-2">
-          <a
-            href="#home"
-            className="text-highlight text-[20px] font-bold"
-            onClick={(e) => {
-              e.preventDefault();
-              handleSmoothScroll("home");
-            }}
-          >
-            Portfolio
-          </a>
-          <p className="text-text text-[16px]">
-            Building the web, one line of code at a time
-            <br />
-            turning ideas into experiences and pixels into purpose.
-          </p>
-        </div>
+    <div className="w-full bg-background2 flex flex-col justify-center items-center border-border2 border-t-2">
+      <div className="w-full flex justify-center items-center py-10">
+        <div className="w-[417px] sm:w-[746px] md:w-[1280px] 3xl:w-[1650px] flex justify-between">
+          <div className="flex flex-col gap-5">
+            <p>Quick Links</p>
+            <div className="flex flex-col gap-2">
+              {links.map((link) => (
+                <a
+                  key={link}
+                  href={`#${link.toLowerCase()}`}
+                  className="text-text hover:text-highlight text-[16px]"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleSmoothScroll(link.toLowerCase());
+                  }}
+                >
+                  {link}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col gap-5">
+            <p>Contacts</p>
+            <div className="flex flex-col gap-1">
+              <p>
+                <span className="text-highlight">Phone: </span>+63 930 629 7787
+              </p>
+              <p>
+                <span className="text-highlight">Email: </span>
+                johnrey.silverio.p@gmail.com
+              </p>
+              <p>
+                <span className="text-highlight">Location: </span>
+                Davao City
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-end text-end gap-5">
+            <Image
+              src="/svg/JR Logo.svg"
+              width={1000}
+              height={1000}
+              alt="JR Logo"
+              className="w-[40px] md:w-[45px] 3xl:w-[55px]"
+            />
 
-        <div className="flex w-full gap-4 justify-center items-center">
-          <a
-            href="https://web.facebook.com/john.rey.jose.silverio"
-            target="_blank"
-          >
-            <FooterButton Icon={FaFacebookF} />
-          </a>
-          <a href="https://www.linkedin.com/in/jrsilverio17/" target="_blank">
-            <FooterButton Icon={FaLinkedinIn} />
-          </a>
-          <a href="https://www.instagram.com/slverio.jr/" target="_blank">
-            <FooterButton Icon={FaInstagram} />
-          </a>
-          <a
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=johnrey.silverio.p@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FooterButton Icon={FaEnvelope} />
-          </a>
-        </div>
-        <div className="w-full justify-center flex bg-[#121212] py-3">
-          <div className="flex flex-col md:flex-row w-[417px] sm:w-[746px] md:w-[1280px] 3xl:w-[1650px] justify-between items-center">
-            <p className="text-text text-center text-[16px]">
-              Copyright ©2025{" "}
-              <a
-                href="#home"
-                className="text-highlight"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleSmoothScroll("home"); // or replace "home" with the actual section ID
-                }}
-              >
-                John Rey J. Silverio
-              </a>
+            <p className="text-text text-[16px]">
+              Building the web, one line of code at a time
+              <br />
+              turning ideas into experiences and pixels into purpose.
             </p>
 
-            <ul className="text-text flex list-none lg:justify-center">
-              {["about", "craft", "career", "contact"].map((section) => (
-                <li key={section}>
-                  <a
-                    href={`#${section}`}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      handleSmoothScroll(section);
-                    }}
-                  >
-                    <Button className="flex justify-center rounded-none items-center p-1 border-b-2 border-transparent hover:border-highlight shadow-none bg-transparent hover:bg-transparent transition-all duration-300 ease-in-out">
-                      <p className={`text-[16px] text-text font-normal`}>
-                        {section.toLocaleUpperCase()}
-                      </p>
-                    </Button>
-                  </a>
-                </li>
-              ))}
-            </ul>
+            <div className="flex gap-5 items-center">
+              <a
+                href="https://web.facebook.com/john.rey.jose.silverio"
+                target="_blank"
+              >
+                <FooterButton Icon={FaFacebookF} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/jrsilverio17/"
+                target="_blank"
+              >
+                <FooterButton Icon={FaLinkedinIn} />
+              </a>
+              <a href="https://www.instagram.com/slverio.jr/" target="_blank">
+                <FooterButton Icon={FaInstagram} />
+              </a>
+              <a
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=johnrey.silverio.p@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FooterButton Icon={FaEnvelope} />
+              </a>
+            </div>
           </div>
+        </div>
+      </div>
+      <div className="w-full bg-[#121212] flex justify-center items-center">
+        <div className="w-[417px] sm:w-[746px] md:w-[1280px] 3xl:w-[1650px] flex justify-center items-center text-center py-10">
+          <p className="text-text text-center text-[16px]">
+            {`Copyright ©2025 `}
+            <a
+              href="#home"
+              className="text-highlight"
+              onClick={(e) => {
+                e.preventDefault();
+                handleSmoothScroll("home");
+              }}
+            >
+              John Rey J. Silverio
+            </a>
+          </p>
         </div>
       </div>
     </div>
