1a6e1af38a258343373c54af3e7d3a454310f407
johnreysilverio <johnrey.silverio.p@gmail.com>
2025-05-21 16:03:22 +0800

    fixed footbar

--- components/buttons/MainButton.tsx
+++ components/buttons/MainButton.tsx
@@ -7,9 +7,9 @@ const MainButton = ({ text = "" }) => {
   return (
     <Button
       variant="default"
-      className="bg-component2 dark:bg-component2 hover:bg-highlight/75 hover:dark:bg-highlight/75 border-1 border-highlight flex justify-center rounded-md px-10 py-6 shadow-md/30"
+      className="bg-component2 dark:bg-component2/50 hover:bg-highlight/50 hover:dark:bg-highlight/50 border-1 border-highlight flex justify-center rounded-md px-10 py-6 shadow-md/30"
     >
-      <p className="text-[20px] 3xl:text-[24px] text-highlight font-medium text-constantText text-center leading-none">
+      <p className="text-[20px] 3xl:text-[24px] text-highlight font-medium text-center leading-none">
         {text}
       </p>
     </Button>
--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -20,26 +20,25 @@ const Footer = () => {
 
   return (
     <div className="w-full bg-background2 flex justify-center border-border2 border-t-2 pt-5 transition-all duration-200">
-      <div className="flex w-full flex-col items-center justify-center gap-5">
-        <div className="flex">
-          <div className="flex flex-col text-center gap-2">
-            <a
-              href="#home"
-              className="text-highlight text-[20px] font-bold"
-              onClick={(e) => {
-                e.preventDefault();
-                handleSmoothScroll("home");
-              }}
-            >
-              Portfolio
-            </a>
-            <p className="text-text text-[16px]">
-              Building the web, one line of code at a time
-              <br />
-              turning ideas into experiences and pixels into purpose.
-            </p>
-          </div>
+      <div className="flex flex-col items-center justify-center gap-5 w-full">
+        <div className="flex flex-col text-center gap-2">
+          <a
+            href="#home"
+            className="text-highlight text-[20px] font-bold"
+            onClick={(e) => {
+              e.preventDefault();
+              handleSmoothScroll("home");
+            }}
+          >
+            Portfolio
+          </a>
+          <p className="text-text text-[16px]">
+            Building the web, one line of code at a time
+            <br />
+            turning ideas into experiences and pixels into purpose.
+          </p>
         </div>
+
         <div className="flex w-full gap-4 justify-center items-center">
           <a
             href="https://web.facebook.com/john.rey.jose.silverio"
@@ -61,40 +60,42 @@ const Footer = () => {
             <FooterButton Icon={FaEnvelope} />
           </a>
         </div>
-        <div className="flex flex-col md:flex-row w-full justify-between items-center px-5 bg-[#121212] py-3">
-          <p className="text-text text-center text-[16px]">
-            Copyright ©2025{" "}
-            <a
-              href="#home"
-              className="text-highlight"
-              onClick={(e) => {
-                e.preventDefault();
-                handleSmoothScroll("home"); // or replace "home" with the actual section ID
-              }}
-            >
-              John Rey J. Silverio
-            </a>
-          </p>
+        <div className="w-full justify-center flex bg-[#121212] py-3">
+          <div className="flex flex-col md:flex-row w-[417px] sm:w-[746px] md:w-[1280px] 3xl:w-[1650px] justify-between items-center">
+            <p className="text-text text-center text-[16px]">
+              Copyright ©2025{" "}
+              <a
+                href="#home"
+                className="text-highlight"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleSmoothScroll("home"); // or replace "home" with the actual section ID
+                }}
+              >
+                John Rey J. Silverio
+              </a>
+            </p>
 
-          <ul className="text-text flex list-none lg:justify-center">
-            {["about", "craft", "career", "contact"].map((section) => (
-              <li key={section}>
-                <a
-                  href={`#${section}`}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleSmoothScroll(section);
-                  }}
-                >
-                  <Button className="flex justify-center rounded-none items-center p-1 border-b-2 border-transparent hover:border-highlight shadow-none bg-transparent hover:bg-transparent transition-all duration-300 ease-in-out">
-                    <p className={`text-[16px] text-text font-normal`}>
-                      {section.toLocaleUpperCase()}
-                    </p>
-                  </Button>
-                </a>
-              </li>
-            ))}
-          </ul>
+            <ul className="text-text flex list-none lg:justify-center">
+              {["about", "craft", "career", "contact"].map((section) => (
+                <li key={section}>
+                  <a
+                    href={`#${section}`}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleSmoothScroll(section);
+                    }}
+                  >
+                    <Button className="flex justify-center rounded-none items-center p-1 border-b-2 border-transparent hover:border-highlight shadow-none bg-transparent hover:bg-transparent transition-all duration-300 ease-in-out">
+                      <p className={`text-[16px] text-text font-normal`}>
+                        {section.toLocaleUpperCase()}
+                      </p>
+                    </Button>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
